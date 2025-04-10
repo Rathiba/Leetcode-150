5ea6b2ab97d7e880b16f35faadd24315ceda89b4
Rathiba <88150060+Rathiba@users.noreply.github.com>
2025-04-10 11:49:10 -0700

    Add solution for IndexOfSubstring problem

---
+++ IndexOfSubstring-28.js
@@ -0,0 +1,7 @@
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+};
+let haystack = "happyday";
+let needle = "happy"
+let result = strStr(haystack, needle);
+console.log(result);
